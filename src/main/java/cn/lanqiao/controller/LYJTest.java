2e78c4fea64b9b51e602e961d99f06eacfa9bd08
yynj-zdj <1930965623@qq.com>
2024-11-12 11:27:15 +0800

    添加YYNJController类及main方法输出

--- src/main/java/cn/lanqiao/controller/LYJTest.java
+++ src/main/java/cn/lanqiao/controller/LYJTest.java
@@ -2,6 +2,6 @@ package cn.lanqiao.controller;
 
 public class LYJTest {
     public static void main(String[] args) {
-        System.out.println("lmx(继承)");
+        System.out.println("lmx(继承)(重写)");
     }
 }
