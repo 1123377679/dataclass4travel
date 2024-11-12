26fd75a7730330ad29afa328c637c4ec08ce8c44
lmx <10632939+try-to-be-better@user.noreply.gitee.com>
2024-11-12 11:16:22 +0800

    lmx xxx业务实现

---
+++ src/main/java/cn/lanqiao/controller/LMXController.java
@@ -0,0 +1,12 @@
+package cn.lanqiao.controller;
+
+/**
+ * @Author: 李某人
+ * @Date: 2024/11/12/11:14
+ * @Description:
+ */
+public class LMXController {
+    public static void main(String[] args) {
+        System.out.println("lmx");
+    }
+}
