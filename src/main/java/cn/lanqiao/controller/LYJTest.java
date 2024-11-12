e294fe969017e4b1a61546580c3e8ac7916e58d8
SonwCity <liyongjie2726@outlook.com>
2024-11-12 11:39:24 +0800

    提交测试

--- src/main/java/cn/lanqiao/controller/LYJTest.java
+++ src/main/java/cn/lanqiao/controller/LYJTest.java
@@ -3,5 +3,6 @@ package cn.lanqiao.controller;
 public class LYJTest {
     public static void main(String[] args) {
         System.out.println("你好");
+        System.out.println("23333");
     }
 }
