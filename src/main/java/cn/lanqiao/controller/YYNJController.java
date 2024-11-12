d0dfb5eb2486c120ee97fbef6e279ec946f4a6db
yynj-zdj <1930965623@qq.com>
2024-11-12 11:32:33 +0800

    超级牛掰

--- src/main/java/cn/lanqiao/controller/YYNJController.java
+++ src/main/java/cn/lanqiao/controller/YYNJController.java
@@ -2,6 +2,6 @@ package cn.lanqiao.controller;
 
 public class YYNJController {
     public static void main(String[] args) {
-    System.out.println("njt妈妈死了");
+    System.out.println("你好");
     }
 }
