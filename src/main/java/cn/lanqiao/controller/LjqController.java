f278673b74bec581dd12954847e01395b4fba400
ljq <3250233786@qq.com>
2024-11-12 11:24:06 +0800

    ljq的测试信息

---
+++ src/main/java/cn/lanqiao/controller/LjqController.java
@@ -0,0 +1,9 @@
+package cn.lanqiao.controller;
+
+import java.sql.SQLOutput;
+
+public class LjqController {
+    public static void main(String[] args) {
+        System.out.println("ljq");
+    }
+}
