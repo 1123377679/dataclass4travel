4c50ab6271f82e58904e1f0e53d790a4f5e775a3
SonwCity <liyongjie2726@outlook.com>
2024-11-12 11:26:47 +0800

    提交测试

--- src/main/java/cn/lanqiao/controller/LYJTest.java
+++ src/main/java/cn/lanqiao/controller/LYJTest.java
@@ -1,7 +1,13 @@
 package cn.lanqiao.controller;
 
+/**
+ * @Author: 李永杰
+ * @Date: 2024/11/12
+ * @Description:
+ */
 public class LYJTest {
     public static void main(String[] args) {
-        System.out.println("lmx(继承)");
+
+        System.out.println("李永杰");
     }
 }
