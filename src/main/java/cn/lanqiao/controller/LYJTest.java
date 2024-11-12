9904fd485d40ca337298c9da3cab842657f406c2
Snowcity <murongqingyu2726@gmail.com>
2024-11-12 11:44:13 +0800

    提交测试

--- src/main/java/cn/lanqiao/controller/LYJTest.java
+++ src/main/java/cn/lanqiao/controller/LYJTest.java
@@ -3,6 +3,5 @@ package cn.lanqiao.controller;
 public class LYJTest {
     public static void main(String[] args) {
         System.out.println("你好");
-        System.out.println("23333");
     }
 }
