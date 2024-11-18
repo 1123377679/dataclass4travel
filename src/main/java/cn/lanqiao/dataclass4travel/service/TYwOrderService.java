d9ec83bca9aed901fcd3861f664778044bcf8303
ASUS <3501306809@qq.com>
2024-11-18 14:40:38 +0800

    订单管理分页查询完成

---
+++ src/main/java/cn/lanqiao/dataclass4travel/service/TYwOrderService.java
@@ -0,0 +1,13 @@
+package cn.lanqiao.dataclass4travel.service;
+
+import cn.lanqiao.dataclass4travel.pojo.TYwOrder;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author ASUS
+* @description 针对表【t_yw_order】的数据库操作Service
+* @createDate 2024-11-15 17:10:20
+*/
+public interface TYwOrderService extends IService<TYwOrder> {
+
+}
