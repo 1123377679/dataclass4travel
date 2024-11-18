bdaddf919fc8f29ee1d0776e73a7f522309e7a70
zyh <877667096@qq.com>
2024-11-18 18:06:18 +0800

    内容管理分页查询完成代码

---
+++ src/main/java/cn/lanqiao/dataclass4travel/mapper/TCmsMessageMapper.java
@@ -0,0 +1,16 @@
+package cn.lanqiao.dataclass4travel.mapper;
+
+import cn.lanqiao.dataclass4travel.pojo.TCmsMessage;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author zyh
+ * @since 2024-11-18
+ */
+public interface TCmsMessageMapper extends BaseMapper<TCmsMessage> {
+
+}
---
+++ src/main/java/cn/lanqiao/dataclass4travel/service/impl/TCmsMessageServiceImpl.java
@@ -0,0 +1,20 @@
+package cn.lanqiao.dataclass4travel.service.impl;
+
+import cn.lanqiao.dataclass4travel.pojo.TCmsMessage;
+import cn.lanqiao.dataclass4travel.mapper.TCmsMessageMapper;
+import cn.lanqiao.dataclass4travel.service.ITCmsMessageService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author zyh
+ * @since 2024-11-18
+ */
+@Service
+public class TCmsMessageServiceImpl extends ServiceImpl<TCmsMessageMapper, TCmsMessage> implements ITCmsMessageService {
+
+}
