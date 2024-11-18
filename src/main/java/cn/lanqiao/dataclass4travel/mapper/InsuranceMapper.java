85e8a6baf06d95d700de68b941a20a6c7ff2d6b7
yynj-zdj <1930965623@qq.com>
2024-11-18 09:23:16 +0800

    只是有个框架什么都没写

---
+++ src/main/java/cn/lanqiao/dataclass4travel/mapper/InsuranceMapper.java
@@ -0,0 +1,7 @@
+package cn.lanqiao.dataclass4travel.mapper;
+
+import cn.lanqiao.dataclass4travel.pojo.TCmsInsurance;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+public interface InsuranceMapper extends BaseMapper<TCmsInsurance> {
+}
---
+++ src/main/java/cn/lanqiao/dataclass4travel/service/InsuranceService.java
@@ -0,0 +1,6 @@
+package cn.lanqiao.dataclass4travel.service;
+
+import cn.lanqiao.dataclass4travel.pojo.TCmsInsurance;
+
+public interface InsuranceService extends Iterable<TCmsInsurance> {
+}
