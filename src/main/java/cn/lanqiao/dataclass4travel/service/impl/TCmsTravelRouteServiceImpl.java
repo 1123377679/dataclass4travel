7425f2c751f1306abb1c33df598b803a65660eca
ZhaangLi <18072416561@163.com>
2024-11-20 10:05:28 +0800

    查询

--- src/main/java/cn/lanqiao/dataclass4travel/service/impl/TCmsTravelRouteServiceImpl.java
+++ src/main/java/cn/lanqiao/dataclass4travel/service/impl/TCmsTravelRouteServiceImpl.java
@@ -3,8 +3,12 @@ package cn.lanqiao.dataclass4travel.service.impl;
 import cn.lanqiao.dataclass4travel.mapper.TravelRouteMapper;
 import cn.lanqiao.dataclass4travel.pojo.TCmsTravelRoute;
 import cn.lanqiao.dataclass4travel.service.TCmsTravelRouteService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TCmsTravelRouteServiceImpl extends ServiceImpl<TravelRouteMapper, TCmsTravelRoute> implements TCmsTravelRouteService {
